378665c0c91be4f306eb93426586100de82f80d2
adriano-rk <adriano.capistano.dev@gmail.com>
2024-08-16 22:58:39 -0400

    Adicionei arquivos via upload

---
+++ Mygit/src/br/com/adrianork/git/Hello_git.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.com.adrianork.git;
+
+/**
+ * exemplo de integração do Eclipse com o Github
+ */
+public class Hello_git {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Github");
+
+	}
+
+}
